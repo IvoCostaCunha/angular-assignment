53a9f43429182abbe8bd0ea8efca023dc169d6ea
IvoCostaCunha <ivo.costacunha@icloud.com>
2022-10-05 11:03:35 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // Define app file declaration
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // These variables can be used with the interpolation
   title = 'assignment-app';
 }
